5a684370f74586a688feb615e3b1dc9d3e450708
veeramanikandan2020 <64081412+veeramanikandan2020@users.noreply.github.com>
2024-02-20 21:25:03 +0530

    DynamicValuesFaker - print message

--- src/test/java/fackerLibrary/DynamicValuesFaker.java
+++ src/test/java/fackerLibrary/DynamicValuesFaker.java
@@ -5,9 +5,13 @@ public class DynamicValuesFaker {
     public void dynamicFaker(){
 
        // Faker facker = new Facker();
-
-        //facker
-
-
+        System.out.println("Faker library files are not imported");
+        System.out.println("Faker library files are not imported");
+        System.out.println("Faker library files are not imported");
+        System.out.println("Faker library files are not imported");
+        System.out.println("Faker library files are not imported");
+        System.out.println("Faker library files are not imported");
+        System.out.println("Faker library files are not imported");
+        System.out.println("Faker library files are not imported");
     }
 }
