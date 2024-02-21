cbb1af8d82b6609bd945176e703d5658067e661e
Veeramanikandan M <veeramanikandan2020@gmail.com>
2024-02-21 16:33:49 +0530

    Facker

--- src/test/java/fackerLibrary/DynamicValuesFaker.java
+++ src/test/java/fackerLibrary/DynamicValuesFaker.java
@@ -1,10 +1,33 @@
 package fackerLibrary;
 
+import com.github.javafaker.Faker;
+
 public class DynamicValuesFaker {
 
     public void dynamicFaker(){
 
-       // Faker facker = new Facker();
+        //Faker facker = new Facker();
+        //facker fack = new facker();
+        @Test
+        public void dynamicValues() {
+
+            Faker fake=new Faker();
+            String fullName = fake.name().fullName();
+            String firstName = fake.name().firstName();
+            String email=fake.internet().emailAddress();
+            String emailID=firstName+"@"+"testleaf.com";
+            System.out.println(fullName+"-"+firstName+"-"+email+emailID);
+            System.out.println(fullName+"-"+firstName+"-"+email+emailID);
+            System.out.println(fullName+"-"+firstName+"-"+email+emailID);
+            System.out.println(fullName+"-"+firstName+"-"+email+emailID);
+            System.out.println(fullName+"-"+firstName+"-"+email+emailID);
+            System.out.println(fullName+"-"+firstName+"-"+email+emailID);
+            System.out.println(fullName+"-"+firstName+"-"+email+emailID);
+
+
+
+
+
 
         //facker
 
--- src/test/java/reqres/GetList.java
+++ src/test/java/reqres/GetList.java
@@ -14,9 +14,13 @@ public class GetList {
         RequestSpecification input = RestAssured.given().when();
         Response resp =
                 input.get("api/users?page=2");
-
         resp.prettyPrint();
+        System.out.println("Details of API Request for");
+        System.out.println("Base URI : https://reqres.in/");
 
+        System.out.println("Page Request for Page no 2");
+
+        System.out.println("Status Code for get is : 200");
         System.out.println("Response Code is :"+resp.statusCode());
 
 
